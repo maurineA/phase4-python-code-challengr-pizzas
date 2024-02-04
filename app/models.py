ae43ca42153f8aa94bd4ad449ff23b35d89c6dc3
maurineA <maurinawambuia@gmail.com>
2024-02-04 23:08:58 +0300

    created the migration and corrected the relationship

--- app/models.py
+++ app/models.py
@@ -1,29 +1,30 @@
-from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # You can change this to your preferred database URL
-db = SQLAlchemy(app)
-# ma = Marshmallow(app)
+
+ 
+db = SQLAlchemy()
+
 
 # Models
 class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
     pizzas = db.relationship('Pizza', secondary='restaurant_pizza', back_populates='restaurants')
 
 class Pizza(db.Model):
+    __tablename__ = 'pizzas'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
     restaurants = db.relationship('Restaurant', secondary='restaurant_pizza', back_populates='pizzas')
 
+
 class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurantpizzas'
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float)
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id')) 
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))  
     pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
-   
