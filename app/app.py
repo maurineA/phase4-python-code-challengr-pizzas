d62936101c3315e8eec1d3722bff573e5be569fc
maurineA <maurinawambuia@gmail.com>
2024-02-05 08:11:28 +0300

    created the get route for restaurant

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response request, jsonify
 from flask_migrate import Migrate
 
-from models import db, Restaurant
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -16,6 +16,12 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    result = [{"id": restaurant.id, "name": restaurant.name, "address": restaurant.address} for restaurant in restaurants]
+    return jsonify(result)
 
 
 if __name__ == '__main__':
